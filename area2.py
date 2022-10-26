1097833941a3960610ca31235f05e88d67d4cbc7
tron01 <abhiak199776@gmail.com>
2022-10-26 15:09:24 +0530

    area2

---
+++ area2.py
@@ -0,0 +1,8 @@
+#1.find area of triangle whoes base height given
+ #2.find area of trinagle whoes 3 sides are given
+ #3.find area of equilateral trinagle
+
+b=int(input("Enter the base :"))
+h=int(input("Enter the Height"))
+c=(b*h)/2
+print("the Area :",c)      
