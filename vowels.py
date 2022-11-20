c9009e1def6ca2315abc83dab09d03c013c550cf
Abhi3070 <69385258+tron01@users.noreply.github.com>
2022-11-20 21:43:28 +0530

    rmv c

--- vowels.py
+++ vowels.py
@@ -1,8 +1,6 @@
 #Form a list of vowels selected from a given word
 v=['a','e','i','o','u','A','E','I','O','U']
-
 w=input("Enter the word:")
-c=0
 s=[i for i in w if i in v]
 print(s)
 print("number of vowels:",len(s))        
