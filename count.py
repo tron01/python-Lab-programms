24eb5d1406577df5419fa308441ae0747209bca6
Abhi3070 <69385258+tron01@users.noreply.github.com>
2022-11-21 06:31:37 +0530

    count word
    count  occurrences of each word in a line of text.

---
+++ count.py
@@ -0,0 +1,5 @@
+#. Count the occurrences of each word in a line of text. 
+
+w=len(input("Enter the text:").split())
+#The split() method splits a string into a list.
+print(" Count the occurrences of each word:",w)
