aa881b227b5ff7096642cf63079cd5c7a4e4bd89
Abhi3070 <69385258+tron01@users.noreply.github.com>
2022-11-21 21:13:45 +0530

    count(i)

--- count.py
+++ count.py
@@ -1,8 +1,7 @@
 #. Count the occurrences of each word in a line of text. 
 
 
+#The split() method splits a string into a list.
 w=(input("Enter the text:").split())
-print(w)
 a={i:w.count(i) for i in w}
 print(a)
-#The split() method splits a string into a list.
