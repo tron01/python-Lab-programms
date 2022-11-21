3561d2aa016273e5edf8b5cae92624089af079af
Abhi3070 <69385258+tron01@users.noreply.github.com>
2022-11-21 07:21:50 +0530

    fix

--- count.py
+++ count.py
@@ -3,9 +3,6 @@
 
 w=(input("Enter the text:").split())
 print(w)
-for i in range(0,len(w)):
-    c=w.count(w[i])
-    print("word: ",w[i],":count :",c)
-
-
+a={i:w.count(i) for i in w}
+print(a)
 #The split() method splits a string into a list.
