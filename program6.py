638d7eea58af5ac06b9c50027578f7bfb8530934
Abhi3070 <69385258+tron01@users.noreply.github.com>
2022-11-30 01:31:02 -0800

    fixed issue

--- program6.py
+++ program6.py
@@ -1,6 +1,7 @@
 #Store a list of first names. Count the occurrences of ‘a’ within the list
-names=["Ashil","Anand","Ajeesh","Aljo"]
+names=["ashil","anand","ajeesh","aljo"]
 count=0
 for name in names:
-    count=count+name.count('A')
-    print("Occ of a in the list", name,"is:",count)
+    count=count+name.count('a')
+    print("occurrences  of a in the list", name,"is:",count)
+    count=0
