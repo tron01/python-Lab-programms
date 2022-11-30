1eb11e40be8ded99d844dfa4f74dbc359de3f56a
Abhi3070 <69385258+tron01@users.noreply.github.com>
2022-11-30 01:51:41 -0800

    7 (a)

---
+++ program7.py
@@ -0,0 +1,13 @@
+#Enter 2 lists of integers. Check
+#  (a) Whether list are of same length 
+# (b)  whether list sums to same value 
+# (c) whether any value occur in both 
+
+l1=[1,2,3,4,5,9]
+l2=[2,3,5,6,7]
+
+if len(l1) == len(l2):
+    print("same length")
+else:
+    print("different length")
+
