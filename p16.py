fd97fd21f641ad0f5a02545ce0d95c4565e1cfc3
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-03 22:32:46 +0530

    p16

---
+++ p16.py
@@ -0,0 +1,12 @@
+#Create a single string separated with space from two strings by swapping the character at position 1.
+s1="Food"
+s2="good"
+print("first string : ",s1)
+print("second string: ",s2)
+x=s1[0:2]
+y=s2[0:2]
+s1=s1.replace(s1[0:2],y)
+s2=s2.replace(s2[0:2],x)
+print("----After swapping----")
+print("first string :- ",s1)
+print("second string :- ",s2)
