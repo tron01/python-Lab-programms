160975fa55a7140b2c9747d74b0a5ca5a69aaa75
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-04 22:01:32 -0800

    fix

--- p15.py
+++ p15.py
@@ -1,6 +1,6 @@
 #. Print out all colors from color-list1 not contained in color-list2.
 l1=["white", "black", "pink"]
 l2=["white", "blue", "red"]
-u=[]
 u = set(l1).difference(l2)
-print(u)
+b=list(u)
+print(b)
--- p16.py
+++ p16.py
@@ -1,5 +1,5 @@
 #Create a single string separated with space from two strings by swapping the character at position 1.
-s1="Food"
+s1="food"
 s2="good"
 print("first string : ",s1)
 print("second string: ",s2)
@@ -8,5 +8,4 @@ y=s2[0:2]
 s1=s1.replace(s1[0:2],y)
 s2=s2.replace(s2[0:2],x)
 print("----After swapping----")
-print("first string :- ",s1)
-print("second string :- ",s2)
+print("string :",s1," ",s2)
