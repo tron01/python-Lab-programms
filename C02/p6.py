ae0d3b84610646706cb623b16acbaa097a75e0fd
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-07 09:50:50 -0800

    Update p6.py

--- C02/p6.py
+++ C02/p6.py
@@ -1,4 +1,12 @@
 #Count the number of characters (character frequency) in a string. 
 
-a="number"
-print("Length of string:",len(a))
+st=input("Enter a word:")
+
+c=0
+for i in st:
+    if i ==" ":
+        continue
+    else:
+        c=c+1
+print(c,"character in "+ st)        
+
