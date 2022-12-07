ac9a7aacc5330400a13dab2edc961b879ec475fc
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-07 10:37:10 -0800

    Update p9.py

--- C02/p9.py
+++ C02/p9.py
@@ -10,3 +10,12 @@ Construct following pattern using nested loop
 * *
 *
 """
+n=int(input("Enter a limit"))
+for i in range(0,n+1):
+    for j in range(0,i):
+        print("*",end=" ")
+    print("\n")
+for i in reversed(range(0,n+1)):
+    for j in reversed(range(0,i)):
+         print("*",end=" ")
+    print("\n")
