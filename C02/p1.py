c35e4aa26fb44d36f3086e713ace4b5701c58a0e
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-07 08:23:28 -0800

    Update p1.py

--- C02/p1.py
+++ C02/p1.py
@@ -1,6 +1,12 @@
 #Program to find the factorial of a number 
 fact=1
 n=int(input("Enter the number:"))
-for i in range(1,n+1):
-    fact=fact*i
-print("factorial of",n,"is:",fact)
+if n <= 0:
+    print("factorial of",n,"is:",fact)
+else:
+    for i in range(1,n+1):
+        fact=fact*i
+    print("factorial of",n,"is:",fact)
+
+
+
