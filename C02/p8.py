311edfc962c477b41f8458d2496d066a4e414d60
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-07 10:40:34 -0800

    fix

--- C02/p8.py
+++ C02/p8.py
@@ -1,3 +1,4 @@
 #Accept a list of words and return length of longest word
 
-n=int(input("Enter the limit"))
+n=int(input("Enter a limit"))
+
--- C02/p9.py
+++ C02/p9.py
@@ -15,7 +15,7 @@ for i in range(0,n+1):
     for j in range(0,i):
         print("*",end=" ")
     print("\n")
-for i in reversed(range(0,n+1)):
+for i in reversed(range(0,n)):
     for j in reversed(range(0,i)):
          print("*",end=" ")
     print("\n")
