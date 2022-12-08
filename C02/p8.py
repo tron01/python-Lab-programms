4941e2dd2879f1b6813d10ab23649c6f85279024
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-08 19:46:57 +0530

    co2 p8

--- C02/p8.py
+++ C02/p8.py
@@ -1,4 +1,16 @@
 #Accept a list of words and return length of longest word
 
-n=int(input("Enter a limit"))
+def longestLength(words):
+    maxLenList = []
+     
+    for word in words:
+        maxLenList.append((len(word), word))
+     
+    maxLenList.sort()
+     
+    print("The word with the longest length is:", maxLenList[-1][1],
+          " and length is ", len(maxLenList[-1][1]))
+ 
 
+a = ["one", "two", "third", "four"]
+longestLength(a)
