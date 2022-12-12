11bb8d8fbc34e726fe99bb410bf29c2447e6cee3
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-11 22:43:50 -0800

    Update p1.py

--- C03/p1.py
+++ C03/p1.py
@@ -1,6 +1,7 @@
 
 #. Work with built-in packages
 
+#1.a
 
 import random
 
@@ -8,6 +9,8 @@ import random
 
 #random.shuffle(mylist)
 #print(mylist)
+
+#1.b
 
 import time
 
@@ -24,6 +27,7 @@ import time
 
 #print("current month :",t.tm_mon)
 
+#1.c
 import calendar
 
 #month =int(input("Enter the month:"))
@@ -32,11 +36,17 @@ import calendar
 #print(calendar.month(year,month))
 #print(calendar.calendar(2022))
 
-
+#1.d
 import math 
 
-print("value of pi :",math.pi)
+#print("value of pi :",math.pi)
+from math  import sqrt ,pi
+#print("square root of 169:" ,sqrt(169))
+#print("value of pi :",math.pi)
+#print(math.cos(90))
+#print(math.sin(45))
 
+#1.c
 import statistics as s
 
 mlist=[1,2,43,5,6,66,77,44]
@@ -45,4 +55,4 @@ print("Mode :",s.mode(mlist))
 print("Median :",s.median(mlist))
 print("Harmonic mean :",s.harmonic_mean(mlist))
 print("Statistics variance:",s.variance(mlist))
-print("Statistics  Median low :",s.median_low(mlist))
+print("Statistics  Median low :",s.median_low([40,55,74,7,-6,78,50]))
