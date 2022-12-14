b02e69f3a97b096e4e299ce11f23b78d90e03b36
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-14 02:01:34 -0800

    Create p3.py

---
+++ C03/p3.py
@@ -0,0 +1,7 @@
+"""
+Create a package graphics with modules rectangle, circle and sub-package 3D-graphics with modules cuboid and sphere. 
+Include methods to find area and perimeter of respective figures in each module. 
+Write programs that finds area and perimeter of figures by different importing statements. 
+(Include selective import of modules and import * statements)
+
+"""
