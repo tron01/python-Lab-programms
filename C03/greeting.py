e5a1715818da00a1439597e91b2904bae230808b
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-14 00:31:54 -0800

    variable passing
    variable passing  using  module

--- C03/greeting.py
+++ C03/greeting.py
@@ -1,3 +1,8 @@
 def greeting(name):
     print(" Hello  "+name)
 
+person1 = {
+    "name":"john",
+    "age":16,
+    "country":"India"
+}
--- C03/p1(mod).py
+++ C03/p1(mod).py
@@ -1,4 +1,4 @@
 import greeting as  g
 
 g.greeting("Harii")
-
+print(g.person1["name"])
