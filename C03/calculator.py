c5005ea20430e4423565c157f0b3b4413f671434
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-14 01:26:09 -0800

    p2 calculator

---
+++ C03/calculator.py
@@ -0,0 +1,17 @@
+def add(n1,n2):
+    res=0
+    res=n1+n2
+    print("Sum : ",res)
+def sub(n1,n2):
+    res=0
+    res=n1 - n2
+    print("Sub : ",res)
+def mult(n1,n2):
+    res=0
+    res=n1*n2
+    print("Mult : ",res)
+def div(n1,n2):
+    res=0
+    res=n1/n2
+    print("div : ",res)
+
--- C03/p2.py
+++ C03/p2.py
@@ -4,3 +4,13 @@ create a calculator module having functions suchs as add,sub,multi and div
 write a program to implement to calculator module 
 
 """
+
+import calculator as c
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+
+c.add(a,b)
+c.sub(a,b)
+c.mult(a,b)
+c.div(a,b)
