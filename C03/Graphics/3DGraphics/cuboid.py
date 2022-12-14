39bb2103a2ed371430a7daf38794edd03ec8ecec
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-14 02:15:49 -0800

    Cuboid,Sphere methods

---
+++ C03/Graphics/3DGraphics/cuboid.py
@@ -0,0 +1,6 @@
+
+def CuboidArea(l,w,h):
+    print("Area of cuboid:",(2*l*w)+(2*l*h)+(2*h*w))
+
+def CuboidPerimeter(l,w,h):
+    print( "Perimeter of  cuboid :",(4*(l+w+h)) )   
---
+++ C03/Graphics/3DGraphics/sphere.py
@@ -0,0 +1,8 @@
+
+from math import pi
+
+def SphereArea(r):
+    return (4*pi*r*r)
+
+def SpherePerimeter(r):
+    return (2*pi*r)    
