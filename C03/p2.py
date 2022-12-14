c04e1e92414f6f68e63121e32c344ad5133a7b6d
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-14 01:47:25 -0800

    ch added

--- C03/p2.py
+++ C03/p2.py
@@ -7,10 +7,24 @@ write a program to implement to calculator module
 
 import calculator as c
 
-a=int(input("Enter the first number:"))
-b=int(input("Enter the second number:"))
+ch=input("select the operation\na.Add\nb.sub\nc.multiply\nd.Divide\nEnter the choice (a/b/c/d): ")
 
-c.add(a,b)
-c.sub(a,b)
-c.mult(a,b)
-c.div(a,b)
+
+if ch =="a":
+    a=int(input("Enter the first number:"))
+    b=int(input("Enter the second number:"))
+    c.add(a,b)
+elif ch =="b":
+    a=int(input("Enter the first number:"))
+    b=int(input("Enter the second number:"))
+    c.sub(a,b) 
+elif ch =="c":
+    a=int(input("Enter the first number:"))
+    b=int(input("Enter the second number:"))
+    c.mult(a,b)
+elif ch =="d":
+    a=int(input("Enter the first number:"))
+    b=int(input("Enter the second number:"))
+    c.div(a,b)
+else:
+    print("Wrong input!")    
