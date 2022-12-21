e14e37a700baedb0ad24a7af5b9cee21c22615b4
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-21 02:17:54 -0800

    b=1
    default value for b

--- C04/sample/p10.py
+++ C04/sample/p10.py
@@ -1,13 +1,17 @@
 #try catch divide by zero
-
+#l =[10,20,30,49,1]
 try:
     a=int(input("Enter A:"))
     b=int(input("Enter B:"))
     c=a/b
+    print(c)
+    #print(l[5])
 
 except Exception as e:
     #print("Can't divide with zero")
-    print(e)
+    b=1
+    c=a/b
+    print(c)
 else:
     print("No Exception")
 finally:
