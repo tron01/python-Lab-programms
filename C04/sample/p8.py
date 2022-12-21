42ff612b2ff5e2ecbb7a1fe0db0f19b7049cb8f4
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-21 01:24:20 -0800

    private method

--- C04/sample/p8.py
+++ C04/sample/p8.py
@@ -14,9 +14,12 @@ obj.display()
 # __a for private
 class emp:
     __a=10
-    def display(self):
+    # __ for private  
+    def __display(self):
         print("Welcome!",self.__a)
-       
+    # __ show will access display! 
+    def show(self):
+        self.__display() 
 obj = emp()
 #print(obj.a)
-obj.display()
+obj.show()
