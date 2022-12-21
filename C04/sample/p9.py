7fc0d4e1115b199844356a0800ee469960db4c6d
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-21 02:01:43 -0800

    Update p9.py

--- C04/sample/p9.py
+++ C04/sample/p9.py
@@ -5,10 +5,16 @@ class Aclass(ABC): #abstract class
     @abstractmethod #decorator
     def display(self):
         None
+    @abstractmethod
+    def show(self):
+        None    
 class Demo(Aclass): #concrete class
     def display(self):
         print("Abstract method")
-
+    def show(self):
+        print(" Abstract show ")
+        
 obj=Demo()
 obj.display()
+obj.show()
 
