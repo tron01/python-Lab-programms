637c55b7e599c3badaed0e35062a8d5c7385b2c7
Abhi3070 <69385258+tron01@users.noreply.github.com>
2023-01-04 15:10:33 +0530

    c04 _p4

---
+++ C04/C04_p4.py
@@ -0,0 +1,33 @@
+#Create a class Time with private attributes hour, minute and second.
+#  Overload ‘+’ operator to find sum of 2 time.
+
+class Time:
+    def __init__(self):
+        self.__h=int(input("Enter the hour:"))
+        self.__m=int(input("Enter the minute:"))
+        self.__s=int(input("Enter the second:"))
+    def __add__(self,time2):
+        hour =self.__h + time2.__h
+
+        minute =self.__m + time2.__m
+        
+        second  =self.__s + time2.__s
+        
+        if minute >=60:
+            hour =hour+1
+            minute=minute-60
+        if second >=60:
+            minute =minute +1 
+            second =second-60   
+        #return second,minute,hour
+        print("sum of Hours:",hour)
+        print("sum of minute:",minute)
+        print("sum of second:",second)         
+
+print("Time of first object")
+time1=Time()
+print("Time of second object")   
+time2=Time()    
+print("-------------------")    
+time1.__add__(time2)
+print("-------------------")    
