321be6bd25af46b59233984d0080126ba640845b
Abhi3070 <69385258+tron01@users.noreply.github.com>
2023-01-11 15:38:34 +0530

    # read multiple line

--- C05/co5_p1.py
+++ C05/co5_p1.py
@@ -11,11 +11,22 @@ print(fn.name) #file name
 
 fo =open("demo2.txt","w")
 
-fo.write("Python is dynamically-typed and garbage-collected. It supports multiple programming paradigms")
+fo.write("Python is dynamically-typed and garbage-collected \n. It supports multiple programming paradigms")
 fo.close()
 
+""" #replace data in file
 f2 =open("demo2.txt","w")
 f2.write("Python gdgg")
 f2.close()
+"""
+
+fn1 =open("demo2.txt","r")
+s1 =fn1.readline()# read multiple line
+s2 =fn1.readline()
+
+print(s1)
+print(s2)
+fn1.close()
+
 
 
