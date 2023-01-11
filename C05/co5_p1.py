43763fbab16139481df43bbc7dcf203abfee2a6e
Abhi3070 <69385258+tron01@users.noreply.github.com>
2023-01-11 15:33:55 +0530

    rewrite

--- C05/co5_p1.py
+++ C05/co5_p1.py
@@ -2,12 +2,20 @@
 
 fn = open("demo.txt",'r') #run in thonny  (filename,mode)
 s= fn.readline() # for read line by line
-print(s) 
+print(s)
+fn.close()
 print(fn.closed)  #file close status
 print(fn.mode) #file mode 
 print(fn.name) #file name
+
 
 fo =open("demo2.txt","w")
 
 fo.write("Python is dynamically-typed and garbage-collected. It supports multiple programming paradigms")
 fo.close()
+
+f2 =open("demo2.txt","w")
+f2.write("Python gdgg")
+f2.close()
+
+
