b3a5eaa56afae44285a405657e5006ea8790e060
tron01 <69385258+tron01@users.noreply.github.com>
2023-01-16 14:18:41 +0530

    import csv  module

--- C05/c05_p3.py
+++ C05/c05_p3.py
@@ -13,6 +13,13 @@ print(id)
 
 """
 #print(csv_f.tail())
-
+"""
 csv_f = pd.read_csv("users.csv",usecols=["id","name"])
 print(csv_f)
+"""
+import csv 
+
+csv_f = open("users.csv")
+csv_reader =csv.reader(csv_f)
+for line in csv_f:
+    print(line)
