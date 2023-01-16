ea5e4b9267fc43981782af8713f60f61a88b0daf
tron01 <69385258+tron01@users.noreply.github.com>
2023-01-16 14:06:37 +0530

    Update c05_p3.py

--- C05/c05_p3.py
+++ C05/c05_p3.py
@@ -2,9 +2,17 @@
 import pandas as pd
 
 
-csv_f = pd.read_csv("users.csv")
+#csv_f = pd.read_csv("users.csv")
 #print(csv_f)
 print()
 #print(csv_f.head())
+"""
+#specfic column
 id=csv_f[["name","batch"]]
 print(id)
+
+"""
+#print(csv_f.tail())
+
+csv_f = pd.read_csv("users.csv",usecols=["id","name"])
+print(csv_f)
