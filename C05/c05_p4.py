e32b0c60f2daea49fa88bdc50f1695d4e850cd07
Abhijith Alias <69385258+tron01@users.noreply.github.com>
2023-01-18 14:30:26 +0530

    write new csv file

--- C05/c05_p4.py
+++ C05/c05_p4.py
@@ -1,1 +1,17 @@
-#Write a Python program to read specific columns of a given CSV file and print the content of the columns.
+#Write a new csv file.
+
+"""
+import csv
+csv_f =open('studentdetails.csv','r')
+
+csv_r =csv.reader(csv_f)
+print(csv_r)
+"""
+
+import csv
+with open('studentdetails.csv','r') as csv_f:
+    csv_r =csv.reader(csv_f)
+    with open('newstudentdetails.csv','w') as csv_f2:
+        csv_w =csv.writer(csv_f2)
+        for line in csv_r:
+            csv_w.writerow(line)
