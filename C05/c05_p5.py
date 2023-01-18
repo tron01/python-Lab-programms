4c1ed3f441d28824884aa80a58b9a0d94c5c2b0f
Abhijith Alias <69385258+tron01@users.noreply.github.com>
2023-01-18 14:54:11 +0530

    fix swap p5 p4

--- C05/c05_p5.py
+++ C05/c05_p5.py
@@ -1,12 +1,2 @@
 #Write a Python program to write a Python dictionary to a csv file. After writing the CSV file read the CSV file and display the content.
 
-
-import csv
-with open('studentdetails.csv','r') as csv_f:
-    csv_r =csv.DictReader(csv_f)
-    with open('studentdetails_Dict.csv','w') as csv_f2:
-        fieldName =["Roll No","Student Name","Grade","CO1","CO2","CO3","CO4","CO5"]
-        csv_w =csv.DictWriter(csv_f2 ,fieldnames=fieldName)
-        csv_w.writeheader()
-        for line in csv_r:
-            csv_w.writerow(line)     
