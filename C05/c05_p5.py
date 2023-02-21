bca92fb29d38f293f6f71e17578ceb3bb1546cd6
tron01 <69385258+tron01@users.noreply.github.com>
2023-02-21 16:39:17 +0530

    co5 p5 fix

--- C05/c05_p5.py
+++ C05/c05_p5.py
@@ -3,11 +3,20 @@
 
 import csv
 
-student =[{'No':1,'name':'Ashil','role':'student'}]
+student =[{'No':1,'name':'joby','role':'student'},
+          {'No':2,'name':'jack','role':'student'},
+          {'No':3,'name':'john','role':'student'}]
 csv_f =open('Name.csv','w')
 field_name= list(student[0].keys())
 
 csv_w  =csv.DictWriter(csv_f,fieldnames=field_name)
-csv_w.writeheader()+
+csv_w.writeheader()
 csv_w.writerows(student)
 csv_f.close()
+
+
+csv_file=open('Name.csv','r')
+csv_reader=csv.reader(csv_file)
+#print (csv_reader)
+for line in csv_file:
+    print (line,end="")
